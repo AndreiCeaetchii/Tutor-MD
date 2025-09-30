5a7d9d25472ac0a8cbeec0815fdefc1625716877
ciprik13 <ciprianmoisenco.it@gmail.com>
2025-09-30 17:58:26 +0300

    feat: update Google OAuth client ID to use environment variable in booking components

--- FrontEnd/src/components/tutor/Bookings/TutorBookings.vue
+++ FrontEnd/src/components/tutor/Bookings/TutorBookings.vue
@@ -199,7 +199,7 @@ const handleAddToGoogleCalendar = (bookingId: number): Promise<void> => {
 
   return new Promise((resolve, reject) => {
     const tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: '425538151525-bhujljp8s9kn9vffkd0rf1cad6gd1epb.apps.googleusercontent.com',
+      client_id: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
       scope: 'https://www.googleapis.com/auth/calendar.events',
 
       callback: async (response: any) => {
